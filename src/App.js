b94c882979e1aec4ccc680539c53536894ef312f
CYNDRICK <CYNDRICK@DESKTOP-BD00HBI>
2026-04-07 14:06:15 +0800

    initial clean portoflio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar/navbar";
+import Intro from "./components/NavBar/Intro/intro";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Intro/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/Intro/intro.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './intro.css';
+import bg from '../../../assets/image.png';
+import btnImg from '../../../assets/hireme.png';
+import  { Link } from "react-scroll";
+
+const Intro = () => {
+    return (
+        <section id="intro">
+            <div className="introContent">
+                <span className="hello">Hello, </span>
+                <span className="introText">I'm <span className="introName">Cynn</span><br/>Software Developer</span>
+                <p className="introPara">I’m a software developer who turns ideas into efficient, scalable <br></br>solutions through clean code and creative problem-solving.</p>
+                <Link><button className="btn"><img src={btnImg} alt="Hire Me" className="btnImg"></img>Hire me</button></Link>
+            </div>
+            <img src={bg} alt="Profile" className="bg"/>
+        </section>
+    );
+}
+
+export default Intro;
